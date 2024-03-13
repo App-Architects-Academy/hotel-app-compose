74c989426fde07604696eb3287f83278260a71aa
patthhar <parthdroidworld@gmail.com>
2024-03-14 02:21:18 +0530

    Add SplashScreen API

--- app/src/main/java/me/darthwithap/hotel_app/MainActivity.kt
+++ app/src/main/java/me/darthwithap/hotel_app/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import me.darthwithap.hotel_app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
+    val splashScreen = installSplashScreen()
     setContent {
       AppTheme {
         // A surface container using the 'background' color from the theme
